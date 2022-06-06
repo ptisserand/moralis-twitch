c2650b65b31059483ed0c6b800243165ceccac98
Patrice Tisserand <p.tisserand@gmail.com>
2022-06-07 00:24:04 +0200

    src/Streamer: add call to cmd/stream.sh

--- src/Streamer.ts
+++ src/Streamer.ts
@@ -1,5 +1,8 @@
+import { spawn } from 'child_process';
+
 class Streamer {
     running: boolean;
+    child: any = null;
 
     constructor() {
         this.running = false;
@@ -11,6 +14,14 @@ class Streamer {
         }
         console.log("Start streaming");
         this.running = true;
+        this.child = spawn("./cmd/stream.sh");
+        this.child.on('error', (error: any) => {
+            console.error(`error: ${error.message}`);
+        });
+
+        this.child.stderr.on('data', (data: any) => {
+            console.log(`stderr: ${data}`);
+        });
     }
 
     async stop() {
@@ -18,6 +29,16 @@ class Streamer {
             return;
         }
         console.log("Stop streaming");
+        if ((null !== this.child) && (false === this.child.killed)) {
+            console.log("Send 'q'")
+            this.child.stdin.write("q");
+            console.log("Send SIGTERM");
+            this.child.kill('SIGTERM');
+            if (false === this.child.killed) {
+                this.child.kill('SIGKILL');
+            }
+            this.child = null;
+        }
         this.running = false;
     }
 }
